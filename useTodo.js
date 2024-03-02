f20f7effe564c0b302f5fdbfee8b4578c389fcdf
pedro <pedro.jcaceresl@gmail.com>
2024-03-02 15:41:24 -0300

    First commit

---
+++ useTodo.js
@@ -0,0 +1,61 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const initialState = [
+    // {
+    // 	id: new Date().getTime(),
+    // 	description: "Study React",
+    // 	done: false,
+    // },
+
+];
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos') || []);
+}
+
+export const useTodo = () => {
+
+    
+    const [todos, dispatchTodo] = useReducer(todoReducer, initialState, init);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
+
+
+    const handleNewTodo = (newTodo) => {
+        const action = {
+            type: "ADD_TODO",
+            payload: newTodo,
+        };
+        dispatchTodo(action);
+    }
+
+    const handleDeleteTodo = (id) => {
+        console.log("the todo id", id);
+        const action = {
+            type: "REMOVE_TODO",
+            payload: id,
+        };
+        dispatchTodo(action);
+    }
+
+    const handleToggleTodo = (id) => {
+        console.log(id, "toggle");
+        const action = {
+            type: "TOGGLE_TODO",
+            payload: id,
+        };
+        dispatchTodo(action);
+    }
+
+    return {
+        handleDeleteTodo,
+        handleNewTodo,
+        handleToggleTodo,
+        pendingTodosCount: todos.filter(todo => !todo.done).length,
+        todos,
+        todosCount: todos.length, 
+    }
+}
